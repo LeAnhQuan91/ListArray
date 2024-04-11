0019d9b8826389b5920591596ccb25e4ac05bba3
LeAnhQuan91 <165478725+LeAnhQuan91@users.noreply.github.com>
2024-04-11 20:19:13 +0700

    add
    add

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> mylists = new List<int>();
+        for(int i=0;i<10;i++)
+        {
+mylists.Add(i);
+        }
+        Console.WriteLine(mylists.Count);
+        foreach(var item in mylists)
+{
+    Console.Write(item+",");
+}
+Console.WriteLine("");
+mylists.RemoveAt(5);
+foreach(var item in mylists)
+{
+    Console.Write(item+",");
+}
+
+        }
+}
+//Count số phần tử bao gồm trong list
+//Add thêm phần từ trong list
+//RemoveAt(index);
+//Clear: xoá toàn bộ
